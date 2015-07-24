10812809acfb8b30520fb0b2a30c91aff8bb92ed
Nick <nickjrose@gmail.com>
2015-07-24 17:02:26 -0400

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -14,7 +14,7 @@ for i in range(0, 1000):
 
 print sum   # 233168
 
-# More efficient approach integrating the sum of natural numbers formula
+# More efficient approach integrating the sum of natural numbers formula: n(n+1)/2
 def sum_all_multiples(multiple, ceiling):
     reduced_ceiling = (ceiling - 1) // multiple  # ceiling - 1 because it's until the ceiling exclusive of last value
     return multiple * (reduced_ceiling * (reduced_ceiling + 1) / 2)
